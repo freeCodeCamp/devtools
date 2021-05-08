4fa88eafd452acf98c0c2f9a1df099b1968252aa
Seth Falco <seth@falco.fun>
2021-05-08 15:24:57 +0200

    Copyright headings

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2020 Seth Falco and Contributors (https://github.com/SethFalco/fccDevTools/graphs/contributors)
+ * Copyright 2021-2021 Seth Falco and Contributors (https://github.com/SethFalco/fccDevTools/graphs/contributors)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
